acc7c2d3fc98507ddedf262539afae8d763cfd64
unknown <arminjafri138386@gmail.com>
2025-12-29 04:05:42 -0800

    2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-# به انتهای فایل اضافه کنید:
+STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/django_static/'
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # آدرس React
 ]
